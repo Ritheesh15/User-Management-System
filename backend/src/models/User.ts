a1e105d5feb87d235858e70b740602571ca3c449
Ritheesh P <132822618+Ritheesh15@users.noreply.github.com>
2024-08-02 19:54:03 +0530

    Create User.ts

---
+++ backend/src/models/User.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  bio?: string;
+}
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  bio?: string;
+}
+
+
+const userSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+});
+
+const User = mongoose.model<IUser>('User', userSchema);
+
+export default User;
