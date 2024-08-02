a8b02e8433fcec7c03b872c098ae98c5e1ebf3e9
Ritheesh P <132822618+Ritheesh15@users.noreply.github.com>
2024-08-02 19:49:39 +0530

    Create db.ts

---
+++ backend/src/config/db.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string, {
+      useUnifiedTopology: true,
+    } as mongoose.ConnectOptions);
+    console.log('MongoDB connected...');
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(`Error: ${err.message}`);
+    } else {
+      console.error('An unknown error occurred');
+    }
+    process.exit(1);
+  }
+};
+
+export default connectDB;
